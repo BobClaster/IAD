09cc81561c2fc6e553280cf1daa23e438499e177
FuckingHacking <navchalkin@ex.ua>
2018-12-13 00:57:49 +0200

    created genetic algoritm class

---
+++ lab_3/main.py
@@ -0,0 +1,13 @@
+
+class GeneticAlgoritm:
+    def __init__(self):
+        pass
+
+    def mutation(self, x1, x2):
+        pass
+
+    def crossover(self):
+        pass
+
+    def genetic(self):
+        pass
