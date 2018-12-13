d3c8c632852b70c24d124febec9c0e52b0d1f871
FuckingHacking <navchalkin@ex.ua>
2018-12-13 09:16:26 +0200

    created class Chromosome, rewrote initial population, rewrote roulette

--- lab_3/main.py
+++ lab_3/main.py
@@ -1,7 +1,33 @@
 import random
+import copy
 
-BINARY = "bin"
-DECIMAL = "dec"
+GENE = "bin"
+PHENOTYPE = "dec"
+
+
+class Chromosome:
+    def __init__(self, phenotype, generation=0, rate=None):
+        self.phenotype = phenotype
+        self.gene = bin(phenotype)
+        self.generation = generation
+        self.conventional_name = ""
+        self.rate = rate
+
+        # set chromosome name
+        self.new_name()
+
+    def new_name(self, old_name=""):
+        self.generation += 1
+        if not old_name:
+            self.conventional_name = f"x{self.generation}-{self.phenotype}-{random.randint(1, 9)}"
+        else:
+            self.conventional_name = f"x{self.generation}-{self.phenotype}-{old_name[-1]}"
+
+    def __str__(self):
+        return self.conventional_name
+
+    def __call__(self, *args, **kwargs):
+        return self.phenotype, self.gene
 
 
 class GeneticAlgorithm:
@@ -9,15 +35,18 @@ class GeneticAlgorithm:
         self.min = 20
         self.max = 40
         self.step = 0.00001
+        self.init_pop = self._initial_population(self.min, self.max, 6)
 
     @staticmethod
-    def _initial_population(init_limit: int, final_limit: int, n: int) -> dict:
-        population = {}
+    def _initial_population(init_limit: int, final_limit: int, n: int) -> list:
+        population = []
+        generation = 0
         for i in range(1, n+1):
             instance = random.randint(init_limit, final_limit)
             if not (instance in population):
-                population[f"x{i}"] = {DECIMAL: instance, BINARY: bin(instance)}
-
+                population.append(
+                    Chromosome(instance, generation)
+                )
         return population
 
     @staticmethod
@@ -25,31 +54,61 @@ class GeneticAlgorithm:
         return z / 7 + z ** 2 + z ** 3
 
     @staticmethod
-    def _roulette(population: dict) -> list:
+    def _roulette(population: list) -> list:
         instances_sum = 0
-        population_rate = {}
+        new_population = []
         roulette = []
         result = []
 
+        # full rate
         for instance in population:
-            instances_sum += population[instance]
+            instances_sum += instance.phenotype
 
+        # set chromosome rate
         for instance in population:
-            population_rate[instance] = round(population[instance] / instances_sum * 100)
+            instance.rate = round(instance.phenotype / instances_sum * 100)
 
-        for instance in population_rate:
-            roulette += [instance] * population_rate[instance]
+        # creating data for roulette
+        for instance in population:
+            roulette += [instance.conventional_name] * instance.rate
 
+        # Natural selection
         for i in range(len(population)):
             result.append(random.choice(roulette))
 
-        return result
+        # creating new population
+        for x_name in result:
+            chromosome = None
+            # If the gene is repeated, copy it
+            for instance in new_population:
+                if x_name == instance.conventional_name:
+                    chromosome = copy.copy(instance)
+                    chromosome.new_name(instance.conventional_name)
+
+            if chromosome:
+                new_population.append(chromosome)
+            else:
+                for instance in population:
+                    if x_name == instance.conventional_name:
+                        new_population.append(instance)
+
+        return new_population
+
+    @staticmethod
+    def _crossover(population: dict) -> list:
+        list_pop = list(population.keys())
+        partners = []
+        for i in range(len(list_pop) // 2):
+            partners.append([
+                list_pop.pop(random.choice(list_pop)),
+                list_pop.pop(random.choice(list_pop)),
+            ])
+        return partners
 
     def mutation(self, x1, x2):
         pass
 
-    def crossover(self):
-        pass
+
 
     def genetic(self):
         pass
@@ -57,4 +116,6 @@ class GeneticAlgorithm:
 
 obj = GeneticAlgorithm()
 a = {"x1": 200, "x2":320, "x3":54, "x4": 76}
-print(obj._initial_population(20, 40, 5))
+# print(obj._initial_population(a))
+# print([i.conventional_name for i in obj.init_pop])
+print([i.conventional_name for i in obj._roulette(obj.init_pop)])
